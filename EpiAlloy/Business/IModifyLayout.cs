e2b2b800228d7d8abc1040c653d864c0e32c1bb7
Mrunal Daftari <MDaftari@rightpoint.com>
2020-06-26 12:43:48 -0400

    Add project files.

---
+++ EpiAlloy/Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using EpiAlloy.Models.ViewModels;
+
+namespace EpiAlloy.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
---
+++ EpiAlloy/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace EpiAlloy.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
